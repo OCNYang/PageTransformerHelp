6001f9213f8c78858dc1e0702b905056740a3b5e
OCN.Yang <yangocn@163.com>
2017-12-07 15:50:09 +0800

    add RotateDownPageTrans,不完善;

---
+++ pagetransformerhelp/src/main/java/com/ocnyang/pagetransformerhelp/cardtransformer/RotateDownPageTransformer.java
@@ -0,0 +1,46 @@
+package com.ocnyang.pagetransformerhelp.cardtransformer;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/*******************************************************************
+ *    * * * *   * * * *   *     *       Created by OCN.Yang
+ *    *     *   *         * *   *       Time:2017/12/7 15:37.
+ *    *     *   *         *   * *       Email address:ocnyang@gmail.com
+ *    * * * *   * * * *   *     *.Yang  Web site:www.ocnyang.com
+ *******************************************************************/
+
+
+public class RotateDownPageTransformer implements ViewPager.PageTransformer {
+    private static final float DEFAULT_MAX_ROTATE = 15.0f;
+    private float mMaxRotate = DEFAULT_MAX_ROTATE;
+
+    @Override
+    public void transformPage(View view, float position) {
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setRotation(mMaxRotate * -1);
+            view.setPivotX(view.getWidth());
+            view.setPivotY(view.getHeight());
+
+        } else if (position <= 1) { // [-1,1]
+
+            if (position < 0)//[0，-1]
+            {
+                view.setPivotX(view.getWidth() * (0.5f + 0.5f * (-position)));
+                view.setPivotY(view.getHeight());
+                view.setRotation(mMaxRotate * position);
+            } else//[1,0]
+            {
+                view.setPivotX(view.getWidth() * 0.5f * (1 - position));
+                view.setPivotY(view.getHeight());
+                view.setRotation(mMaxRotate * position);
+            }
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setRotation(mMaxRotate);
+            view.setPivotX(view.getWidth() * 0);
+            view.setPivotY(view.getHeight());
+        }
+    }
+}
